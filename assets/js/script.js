e1284d9898e359bc9a771c5ce1726f825bb7a118
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-07-29 15:27:32 +0000

    feat: add processoptions function to post options data correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,8 +25,24 @@ function displayData(data){
     resultsModal.show();
 };
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
